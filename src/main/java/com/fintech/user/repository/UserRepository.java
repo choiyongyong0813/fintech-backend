9d4ae6f72c761cfba704ab3b745fcbc8524c0bd1
최재용 <choiyongyong0813@gmail.com>
2025-07-27 02:24:02 +0900

    충돌수정

--- src/main/java/com/fintech/user/repository/UserRepository.java
+++ src/main/java/com/fintech/user/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package com.fintech.user.repository;
 
-public interface UserRepository {
+import com.fintech.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+//User 엔티티에 접근하기 위한 JPA 레포지토리 인터페이스입니다.
+//기본 CRUD 기능을 포함하며, 이메일로 사용자 검색 기능도 정의합니다.
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 이메일로 사용자 조회 (회원가입 중복 확인 및 로그인에 사용)
+    Optional<User> findByEmail(String email);
 }
